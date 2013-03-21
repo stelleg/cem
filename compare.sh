247884c80ce418c88fe3b1e4db5c5048dd045eed
George Stelle <stelleg@gmail.com>
2013-03-21 14:13:12 -0600

    Added comparison script

---
+++ compare.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+tests="exp-true 
+       exp-false 
+       fact-true"
+
+for test in ${tests}; do
+  echo "Running ${test}"
+  cem=`{ time -p ./cem ./test/${test}.lc > /dev/null; } 2>&1 | grep real | awk '{print $2}'`
+  echo "cem took $cem seconds"
+  ghc=`{ time -p ./test/hs/${test} > /dev/null; } 2>&1 | grep real | awk '{print $2}'`
+  echo "ghc took $ghc seconds" 
+  ratio=`echo "scale=2; $ghc/$cem" | bc`
+  echo "cem is $ratio faster"
+
+done;
+      
+
