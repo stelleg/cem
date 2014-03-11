1b74a75f647b4291513cb9ae6916ae8987d9521e
George Stelle <gwstell@sandia.gov>
2014-03-11 01:48:24 -0600

    Added stdin as special filename -

--- Main.hs
+++ Main.hs
@@ -19,20 +19,22 @@ parseArgs (('-':o:opts):sources) = parseOpts (o:opts) sources
 parseArgs sources = parseOpts "lc" sources
 
 parseOpts :: String -> [String] -> IO ()
-parseOpts [] [] = compile "a.out" =<< getContents
 parseOpts ('l':o) sources = do srclibs <- libs; parseOpts o (srclibs ++ sources)
 parseOpts "v" _ = version
 parseOpts "h" _ = usage
 parseOpts "a" sources = cfa =<< readSources sources
-parseOpts "c" sources = compile (takeWhile (/='.') . last $ sources) =<< readSources sources
+parseOpts "c" sources = compile (chooseOutput . last $ sources) =<< readSources sources
 parseOpts "f" sources = freevars =<< readSources sources
 parseOpts "g" sources = graph =<< readSources sources
 parseOpts "r" sources = partial (\s->return()) =<< readSources sources 
 parseOpts "t" sources = partial (\((c,e),h,s)->print c) =<< readSources sources
 parseOpts _ _ = usage
 
+chooseOutput "-" = "a.out"
+chooseOutput fname = takeWhile (/= '.') fname
+
 readSources [] = getContents
-readSources s = concat <$> mapM readFile s
+readSources s = concat <$> mapM (\f->if f == "-" then getContents else readFile f) s
 
 version = putStrLn $ "0.1 Alpha"  
 usage = putStrLn $ "Usage: cem {-lhvdgpcf} {file(s)}"
